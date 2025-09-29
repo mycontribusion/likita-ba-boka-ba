a7b9958ea47b5b27e161db9b99a23a9f2422be46
Ahmad Musa <mycontribusion01@gmail.com>
2025-09-29 16:43:57 +0100

    update email and linkedin

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,14 +7,13 @@ const Footer = () => {
         <div className="footer-content">
           
           <div className="footer-section">
-            <h3>Game da Lafiya</h3>
+            <h3>Game da mu</h3>
             <p>Muna ba da ilimin kiwon lafiya da Hausa don taimakawa al'umma su sami ilimi mai amfani game da lafiya.</p>
           </div>
           
           <div className="footer-section">
             <h3>Hanyoyin Tuntuɓa</h3>
-            <p>📧 info@lafiya.com</p>
-            <p>📱 +234 123 456 789</p>
+            <p>📧 ahmadmusa1114@gmail.com</p>
             <p>📍 Kano, Najeriya</p>
           </div>
           
@@ -22,14 +21,13 @@ const Footer = () => {
             <h3>Hanyoyin Zamantakewa</h3>
             {/* Using a fragment (<>) or <div> to wrap the list of links */}
             <div>
-              <p><a href="#">Facebook</a></p>
+              <p><a href="https://www.linkedin.com/in/ahmad-m-musa-b93587156/">Linkedin</a></p>
               <p><a href="#">Twitter</a></p>
-              <p><a href="#">WhatsApp</a></p>
             </div>
           </div>
           
         </div>
-        <p>&copy; 2024 Lafiya. Dukkan hakkokin an kiyaye su.</p>
+        <p>&copy; 2024 Likita-ba-boka-ba.</p>
       </div>
     </footer>
   );
