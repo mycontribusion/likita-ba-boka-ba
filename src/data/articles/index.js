4e79c3169c56d61fae899adb292d1c18eb025c98
Ahmad Musa <mycontribusion01@gmail.com>
2026-02-13 18:09:45 +0100

    typhoid malaria

--- src/data/articles/index.js
+++ src/data/articles/index.js
@@ -1,10 +1,11 @@
 import ciwonCiki from './ciwonCiki';
-
+import typhoidMalaria from './typhoidMalaria';
 import wayeLikita from './wayeLikita';
 
 const articlesData = [
   ciwonCiki,
-  wayeLikita
+  wayeLikita,
+  typhoidMalaria
 ];
 
 export default articlesData;
---
+++ src/data/articles/typhoidMalaria.js
@@ -0,0 +1,52 @@
+const typhoidMalaria = {
+  id: "typhoidmalaria",
+  icon: "🦟",
+  category: "cuta",
+  title: "Malaria da Typhoid: Hassan da Hussaini ko Jamila da Jamilu?",
+  excerpt: "Malaria da Typhoid ba 'yan-biyu bane, kuma ba mata da miji bane.",
+  date: "2026-02-13",
+  views: 350,
+  fullContent: `
+  Da sunan Allah nake farawa.
+  
+  Sau da yawa idan mutum yana jin zazzabi, sai ace yanada "Malaria ce da Typhoid." Shin da gaske wadannan cututtukan abokan juna ne?
+  
+  ### Amsa a takaice:
+  A'a.
+  
+  Kana cewa likita ne sauro ya cije ka, in kace Malaria ya kama ka, ko kuma kaci kashin wani in ka cewa likita Typhoid ya kama ka. Eh Kashi na toilet, ba 'kashi ba. Kenan Malaria ba tada alaka ta kusa ko ta nesa da typhoid.
+  
+  Bugu da kari, yayin da Malaria ke fasa kwayoyin halittar dake cikin jini, typhoid 'ya'yan hanji yake fasawa.
+  
+  Kamanceceniyar da take tsakanin su bata wuce ace duk su biyun suna zuwa da zazzabi ba, sannan dukkaninsu basu damun mutum har ace tsawon wata daya ba.
+
+  ### To ai anyi Test
+  
+  Widal wai? Ai ba ta banbancewa tsakanin mutum mai Typhoid, da wanda ya taba yin Typhoid shekaru aru-aru baya. Duk akwai zata nuna.
+  
+  ### Zazzabi
+  
+  Zazzabi akaran kanshi ba cuta bane, yana alamta cuta ne. Sannan ba iya Malaria da Typhoid ne suke sa zazzabi ba. Akwai cututtuka sama da dari masu za zazzabi.
+  
+  Cuta na iya kasancewa ko ina sannan yasa zazzabi,
+  
+  - yana iya kasancewa a ka, ya sa zazzabi
+  - yana iya kasancewa a baki, ya sa zazzabi
+  - yana iya kasancewa a kunne, ya sa zazzabi
+  - yana iya kasancewa a makwogaro, ya sa zazzabi
+  - ...
+  - yana iya kasancewa a dan yatsar kafa, ya sa zazzabi
+  
+  ### A takaice
+  
+  - Zazzabi na iya kasancewa ba daga Malaria da Typhoid ba
+  - Ana iya kamuwa da Malaria, shi kadai
+  - Ana iya kamuwa da Typhoid, shi kadai
+  - Ana iya kamuwa da Malaria da Typhoid, tare
+
+  
+  Allah Ya kara mana lafiya. Ameen.
+  `
+};
+
+export default typhoidMalaria;
